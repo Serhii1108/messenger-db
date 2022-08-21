eb1cf1f6358890636ea6a8ba4fcc8076a96c1176
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-08-21 14:12:40 +0300

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { dataSource } from './ormconfig.js';
+
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot(dataSource.options),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
